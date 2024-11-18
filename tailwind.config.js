6134bf99dc1131b8fa755d33dcea06dd589f9277
aldair.gutierrez <programador1@tecnologicocomfenalco.edu.co>
2024-11-18 15:29:32 -0500

    Update dependencias y configuración basica

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{astro,js,ts,jsx,tsx,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: [
+          "Menlo",
+          "Monaco",
+          "Lucida Console",
+          "Liberation Mono",
+          "DejaVu Sans Mono",
+          "Bitstream Vera Sans Mono",
+          "Courier New",
+          "monospace",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
