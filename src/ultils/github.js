287782049c137199f5bd63a47ed6628341f3f149
aldair.gutierrez <programador1@tecnologicocomfenalco.edu.co>
2024-11-18 23:02:50 -0500

    Update Card.projects

--- src/ultils/github.js
+++ src/ultils/github.js
@@ -1,20 +1,20 @@
 export async function getGitHubRepos(username) {
-  const excluir = ["2A2G", "STUDY_BLOCK"];
+  let excluir = ["2A2G", "STUDY_BLOCK"];
 
-  try {
-    const response = await fetch(
-      `https://api.github.com/users/${username}/repos`
-    );
-
-    if (!response.ok) {
-      throw new Error("Error al obtener los repositorios");
-    }
-
-    const repos = await response.json();
-    const reposFiltrados = repos.filter((repo) => !excluir.includes(repo.name));
-    return reposFiltrados;
-  } catch (error) {
-    console.error(error);
-    throw new Error("Error al obtener los repositorios");
+  if (!username) {
+    throw new Error("El nombre de usuario es requerido");
   }
+  const response = await fetch(
+    `https://api.github.com/users/${username}/repos`
+  );
+
+  if (!response.ok) {
+    throw new Error(
+      `Error al obtener los repositorios: ${response.statusText}`
+    );
+  }
+
+  const repos = await response.json();
+  const reposFiltrados = repos.filter((repo) => !excluir.includes(repo.name));
+  return reposFiltrados;
 }
